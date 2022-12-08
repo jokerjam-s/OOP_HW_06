59cd2a382741047638982f7b2287ed475d6841f5
Андрей <40392418+jokerjam-s@users.noreply.github.com>
2022-12-08 18:02:46 +0300

    start calc

---
+++ Calculator/Calculator/CalcHistory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    /**
+     * История вычислений
+     */
+
+    internal class CalcHistory
+    {
+        private String inputString { get; set; }
+
+        private string result { get; set; }
+
+        public CalcHistory() { }
+
+        public CalcHistory(String inputString ) 
+        { 
+            this.inputString = inputString;
+        }
+    }
+}
